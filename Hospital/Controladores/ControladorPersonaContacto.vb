d0802ec5455bd2cb61751b2c0e4b25ba935d0a52
josé alberto mata mena <mata649@hotmail.com>
2020-08-12 15:09:47 -0600

    dfsdsfd

--- Hospital/Controladores/ControladorPersonaContacto.vb
+++ Hospital/Controladores/ControladorPersonaContacto.vb
@@ -3,10 +3,12 @@
     Function registrar(personaContacto As PersonaContacto) As Boolean
         Try
             Using Almacen As New ProyectoEntities1
-                Almacen.sp_registra
-            End Using
-        Catch ex As Exception
+                Almacen.sp_registrar_persona_contacto(personaContacto.cedula, personaContacto.nombreCompleto, personaContacto.relacionFamiliar, personaContacto.telefono, personaContacto.direccion, personaContacto.cedulaPaciente)
 
+            End Using
+            Return True
+        Catch ex As Exception
+            Return False
         End Try
     End Function
 End Class
