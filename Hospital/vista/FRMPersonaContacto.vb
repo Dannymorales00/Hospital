b33ff4220b25b1cba9fa0250bf38b2f9be92bc47
josé alberto mata mena <mata649@hotmail.com>
2020-08-11 18:13:06 -0600

    sss

--- Hospital/vista/FRMPersonaContacto.vb
+++ Hospital/vista/FRMPersonaContacto.vb
@@ -42,7 +42,7 @@
         labelNombre.Text = "******"
         telefonoTxt.Text = ""
         direccionTxt.Text = ""
-        relacionTxt.Text = ""
+        relacionTxt.Text = "a"
     End Sub
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
