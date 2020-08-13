c11facacde230363899fcb4d6b783078daca0eb4
Leandro Campos Aguilar <leandro-1018@hotmail.com>
2020-08-13 17:22:26 -0600

    FrmCita

--- Hospital/vista/FrmCita.vb
+++ Hospital/vista/FrmCita.vb
@@ -1,14 +1,15 @@
 ﻿Public Class FrmCita
     Private cita As Citas
-    Private buscarPaciente As BuscarPaciente
+    Private buscarPaciente As FrmBuscarPaciente
+    Private buscarMedico As BuscarMedico
     Private paciente As New Paciente
     Private medico As New Medico
 
 
     Public Sub cargarPaciente()
-        Dim pacienteForm As New BuscarPaciente
+        Dim pacienteForm As New FrmBuscarPaciente
         pacienteForm.ShowDialog()
-        paciente = pacienteForm.pacienteRetorno
+        paciente = pacienteForm.PacienteRetorno1
         pacienteForm.Close()
 
         If Not paciente.cedula = Nothing Then
@@ -17,7 +18,19 @@
         End If
     End Sub
 
+    Public Sub cargarMedico()
+        Dim medicoForm As New BuscarMedico
+        medicoForm.ShowDialog()
+        medico = medicoForm.P_Medico
+        medicoForm.Close()
+
+        If Not medico.cedula = Nothing Then
+            LblMedico.Text = medico.cedula.ToString
+        End If
+
+    End Sub
     Private Sub BtnMedico_Click(sender As Object, e As EventArgs) Handles BtnMedico.Click
+        cargarMedico()
 
     End Sub
 
