f3d22338c9650b28b634a71435d3b0c2da6530a0
Leandro Campos Aguilar <leandro-1018@hotmail.com>
2020-08-27 00:04:02 -0600

    FrmExamenes agregar FrmExamenSangre

--- Hospital/vista/FrmExamenes.Designer.vb
+++ Hospital/vista/FrmExamenes.Designer.vb
@@ -24,31 +24,40 @@ Partial Class FrmExamenes
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmExamenes))
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.BtnExamenOrina = New System.Windows.Forms.Button()
         Me.BtnExamenSangre = New System.Windows.Forms.Button()
         Me.PanelExamenes = New System.Windows.Forms.Panel()
-        Me.SalirToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MenuStrip1.SuspendLayout()
         Me.PanelExamenes.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
         '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
         Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SalirToolStripMenuItem})
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
-        Me.MenuStrip1.Size = New System.Drawing.Size(872, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1163, 28)
         Me.MenuStrip1.TabIndex = 0
         Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'SalirToolStripMenuItem
+        '
+        Me.SalirToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
+        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
+        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(52, 24)
+        Me.SalirToolStripMenuItem.Text = "Salir"
         '
         'BtnExamenOrina
         '
         Me.BtnExamenOrina.BackgroundImage = CType(resources.GetObject("BtnExamenOrina.BackgroundImage"), System.Drawing.Image)
         Me.BtnExamenOrina.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.BtnExamenOrina.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.BtnExamenOrina.Location = New System.Drawing.Point(39, 84)
+        Me.BtnExamenOrina.Location = New System.Drawing.Point(52, 103)
+        Me.BtnExamenOrina.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BtnExamenOrina.Name = "BtnExamenOrina"
-        Me.BtnExamenOrina.Size = New System.Drawing.Size(154, 130)
+        Me.BtnExamenOrina.Size = New System.Drawing.Size(205, 160)
         Me.BtnExamenOrina.TabIndex = 1
         Me.BtnExamenOrina.Text = "Examen de Orina"
         Me.BtnExamenOrina.TextAlign = System.Drawing.ContentAlignment.BottomCenter
@@ -60,9 +69,10 @@ Partial Class FrmExamenes
         Me.BtnExamenSangre.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.BtnExamenSangre.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.BtnExamenSangre.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
-        Me.BtnExamenSangre.Location = New System.Drawing.Point(397, 84)
+        Me.BtnExamenSangre.Location = New System.Drawing.Point(529, 103)
+        Me.BtnExamenSangre.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.BtnExamenSangre.Name = "BtnExamenSangre"
-        Me.BtnExamenSangre.Size = New System.Drawing.Size(154, 130)
+        Me.BtnExamenSangre.Size = New System.Drawing.Size(205, 160)
         Me.BtnExamenSangre.TabIndex = 2
         Me.BtnExamenSangre.Text = "Examen de Sangre"
         Me.BtnExamenSangre.TextAlign = System.Drawing.ContentAlignment.BottomCenter
@@ -72,27 +82,22 @@ Partial Class FrmExamenes
         '
         Me.PanelExamenes.Controls.Add(Me.BtnExamenOrina)
         Me.PanelExamenes.Controls.Add(Me.BtnExamenSangre)
-        Me.PanelExamenes.Location = New System.Drawing.Point(141, 58)
+        Me.PanelExamenes.Location = New System.Drawing.Point(188, 71)
+        Me.PanelExamenes.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.PanelExamenes.Name = "PanelExamenes"
-        Me.PanelExamenes.Size = New System.Drawing.Size(595, 339)
+        Me.PanelExamenes.Size = New System.Drawing.Size(793, 417)
         Me.PanelExamenes.TabIndex = 3
-        '
-        'SalirToolStripMenuItem
-        '
-        Me.SalirToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right
-        Me.SalirToolStripMenuItem.Name = "SalirToolStripMenuItem"
-        Me.SalirToolStripMenuItem.Size = New System.Drawing.Size(41, 20)
-        Me.SalirToolStripMenuItem.Text = "Salir"
         '
         'FrmExamenes
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(872, 465)
+        Me.ClientSize = New System.Drawing.Size(1163, 572)
         Me.ControlBox = False
         Me.Controls.Add(Me.PanelExamenes)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "FrmExamenes"
         Me.Text = "FrmExamenes"
         Me.MenuStrip1.ResumeLayout(False)
