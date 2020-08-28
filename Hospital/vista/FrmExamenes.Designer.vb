e910f11197540d1de50369bffac22b6a067c60b2
Leandro Campos Aguilar <leandro-1018@hotmail.com>
2020-08-28 09:50:13 -0600

    Pequeños cambios

--- Hospital/vista/FrmExamenes.Designer.vb
+++ Hospital/vista/FrmExamenes.Designer.vb
@@ -28,6 +28,8 @@ Partial Class FrmExamenes
         Me.BtnExamenOrina = New System.Windows.Forms.Button()
         Me.BtnExamenSangre = New System.Windows.Forms.Button()
         Me.PanelExamenes = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         Me.MenuStrip1.SuspendLayout()
         Me.PanelExamenes.SuspendLayout()
         Me.SuspendLayout()
@@ -55,11 +57,10 @@ Partial Class FrmExamenes
         Me.BtnExamenOrina.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.BtnExamenOrina.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.BtnExamenOrina.Location = New System.Drawing.Point(52, 103)
-        Me.BtnExamenOrina.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.BtnExamenOrina.Margin = New System.Windows.Forms.Padding(4)
         Me.BtnExamenOrina.Name = "BtnExamenOrina"
         Me.BtnExamenOrina.Size = New System.Drawing.Size(205, 160)
         Me.BtnExamenOrina.TabIndex = 1
-        Me.BtnExamenOrina.Text = "Examen de Orina"
         Me.BtnExamenOrina.TextAlign = System.Drawing.ContentAlignment.BottomCenter
         Me.BtnExamenOrina.UseVisualStyleBackColor = True
         '
@@ -70,23 +71,44 @@ Partial Class FrmExamenes
         Me.BtnExamenSangre.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.BtnExamenSangre.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
         Me.BtnExamenSangre.Location = New System.Drawing.Point(529, 103)
-        Me.BtnExamenSangre.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.BtnExamenSangre.Margin = New System.Windows.Forms.Padding(4)
         Me.BtnExamenSangre.Name = "BtnExamenSangre"
         Me.BtnExamenSangre.Size = New System.Drawing.Size(205, 160)
         Me.BtnExamenSangre.TabIndex = 2
-        Me.BtnExamenSangre.Text = "Examen de Sangre"
         Me.BtnExamenSangre.TextAlign = System.Drawing.ContentAlignment.BottomCenter
         Me.BtnExamenSangre.UseVisualStyleBackColor = True
         '
         'PanelExamenes
         '
+        Me.PanelExamenes.Controls.Add(Me.Label2)
+        Me.PanelExamenes.Controls.Add(Me.Label1)
         Me.PanelExamenes.Controls.Add(Me.BtnExamenOrina)
         Me.PanelExamenes.Controls.Add(Me.BtnExamenSangre)
         Me.PanelExamenes.Location = New System.Drawing.Point(188, 71)
-        Me.PanelExamenes.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.PanelExamenes.Margin = New System.Windows.Forms.Padding(4)
         Me.PanelExamenes.Name = "PanelExamenes"
         Me.PanelExamenes.Size = New System.Drawing.Size(793, 417)
         Me.PanelExamenes.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(77, 267)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(153, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Examen de Orina"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(552, 267)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(166, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Examen de Sangre"
         '
         'FrmExamenes
         '
@@ -97,12 +119,13 @@ Partial Class FrmExamenes
         Me.Controls.Add(Me.PanelExamenes)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Margin = New System.Windows.Forms.Padding(4)
         Me.Name = "FrmExamenes"
         Me.Text = "FrmExamenes"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
         Me.PanelExamenes.ResumeLayout(False)
+        Me.PanelExamenes.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -113,4 +136,6 @@ Partial Class FrmExamenes
     Friend WithEvents BtnExamenSangre As Button
     Friend WithEvents PanelExamenes As Panel
     Friend WithEvents SalirToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
 End Class
